cec22901367fa266e59b84b7f02f882e6b29f177
tchuang928 <tc.huang928@gmail.com>
2017-05-30 01:06:18 -0700

    added scrolling animations

--- src/index.js
+++ src/index.js
@@ -1,34 +1,61 @@
-$(document).ready($ => {
-	'use strict'
+'use strict'
+
+$(document).ready(() => {
 	
-		if ($(document).scrollTop() > 50) {
-			$('nav').addClass('scrollFunc');
-		} else {
-			$('nav').removeClass('scrollFunc');
-		}
-
-
-	$(window).scroll(() => {
-		if ($(document).scrollTop() > 50) {
-			$('nav').addClass('scrollFunc');
-		} else {
-			$('nav').removeClass('scrollFunc');
-		}
-	})
-
 	// smooth scrolling
-  $("a").on('click', function(event) {
+  $('a').on('click', function(event) {
 
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
 
       $('html, body').animate({
-        scrollTop: $(hash).offset().top -130
-      }, 1000, function(){
-   
+				// create variable scrollTop number when scrolling from the top due to hide and show subtitles
+        scrollTop: $(hash).offset().top -107
+			}, 1000, () => {
         window.location.hash = hash;
       });
     } 
   });
+
+	// detect scroll position and run proper animation
+	if ($(document).scrollTop() > 50) {
+		$('nav').addClass('scrollFunc');
+	} else {
+		$('nav').removeClass('scrollFunc');
+	}
+
+	// navbar animation on scroll
+	$(window).scroll(() => {
+		if ($(document).scrollTop() > 50) {
+			$('nav').removeClass('navPadding');
+			$('nav').addClass('scrollFunc');
+		} else {
+			$('nav').addClass('navPadding');
+			$('nav').removeClass('scrollFunc');
+		}
+	})
+
+	// about section subtitle animation on scroll
+	$(window).scroll(() => {
+		if ($(document).scrollTop() > 792) {
+			//$('#aboutTitle').addClass('joinNavbar').addClass('navbar-fixed-top');
+			$('#aboutNavbar').fadeIn('slow');
+			$('#aboutTitle').slideUp('slow');
+				$('#aboutPadding').slideDown('slow');
+
+		} else {
+			//$('#aboutTitle').removeClass('joinNavbar').removeClass('navbar-fixed-top');
+			$('#aboutNavbar').hide();
+			$('#aboutTitle').slideDown('slow');
+			$('#aboutPadding').slideUp('slow');
+		}
+	})
+
+	// detect scroll position
+	$(window).scroll(function (event) {
+			var scroll = $(window).scrollTop();
+			console.log(scroll);
+	});
+
 });
