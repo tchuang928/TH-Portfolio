b2b233c0691df966ad109d3725ccc1203dab0b2e
tchuang928 <tc.huang928@gmail.com>
2017-05-31 00:58:14 -0700

    improved scrolling animation

--- src/index.js
+++ src/index.js
@@ -1,61 +1,63 @@
 'use strict'
 
-$(document).ready(() => {
-	
-	// smooth scrolling
-  $('a').on('click', function(event) {
-
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-
-      $('html, body').animate({
-				// create variable scrollTop number when scrolling from the top due to hide and show subtitles
-        scrollTop: $(hash).offset().top -107
-			}, 1000, () => {
-        window.location.hash = hash;
-      });
-    } 
-  });
-
-	// detect scroll position and run proper animation
-	if ($(document).scrollTop() > 50) {
-		$('nav').addClass('scrollFunc');
-	} else {
+function navbarAnimation() {
+	if ($(document).scrollTop() < 50) {
+		$('nav').addClass('navPadding');
 		$('nav').removeClass('scrollFunc');
+	} else {
+		$('nav').removeClass('navPadding');
+		$('nav').addClass('scrollFunc');
 	}
+}
+
+// content section subtitle animation on scroll
+function contentSubtitleAnimation() {
+	
+	let position = $(document).scrollTop();
+	let nav_height = $('nav').outerHeight();
+
+	$('section').each(function(index, value) {
+		let top = $(this).offset().top - nav_height - 235;
+		let bottom = top + $(this).outerHeight();
+
+		if (position >= top && position <= bottom) {
+			$(this).children('.joinNavbar').fadeIn(1000)
+			$(this).children('.title').slideUp('fast')
+			$(this).children('.addPadding').slideDown('fast')
+		} else {
+			$(this).children('.joinNavbar').fadeOut('fast')
+			$(this).children('.title').slideDown('fast')
+			$(this).children('.addPadding').slideUp('fast')
+		}
+	});
+}
+
+
+$(document).ready(() => {
+	navbarAnimation();
+	contentSubtitleAnimation();
+
+	// smooth scrolling
+	$('a').click(function () {
+		var $href = $(this).attr('href');
+		$('body').stop().animate({
+			scrollTop: $($href).offset().top - 107
+		}, 1000);
+		return false;
+	});
+
 
 	// navbar animation on scroll
 	$(window).scroll(() => {
-		if ($(document).scrollTop() > 50) {
-			$('nav').removeClass('navPadding');
-			$('nav').addClass('scrollFunc');
-		} else {
-			$('nav').addClass('navPadding');
-			$('nav').removeClass('scrollFunc');
-		}
-	})
-
-	// about section subtitle animation on scroll
-	$(window).scroll(() => {
-		if ($(document).scrollTop() > 792) {
-			//$('#aboutTitle').addClass('joinNavbar').addClass('navbar-fixed-top');
-			$('#aboutNavbar').fadeIn('slow');
-			$('#aboutTitle').slideUp('slow');
-				$('#aboutPadding').slideDown('slow');
-
-		} else {
-			//$('#aboutTitle').removeClass('joinNavbar').removeClass('navbar-fixed-top');
-			$('#aboutNavbar').hide();
-			$('#aboutTitle').slideDown('slow');
-			$('#aboutPadding').slideUp('slow');
-		}
-	})
+		navbarAnimation();
+		contentSubtitleAnimation();
+	});
 
 	// detect scroll position
-	$(window).scroll(function (event) {
+	/*	
+	$(window).scroll((event) => {
 			var scroll = $(window).scrollTop();
 			console.log(scroll);
 	});
-
+	*/
 });
