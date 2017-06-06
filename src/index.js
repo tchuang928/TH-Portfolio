132bd5feb4d9d25cd1b65927751f4e2dba06ae90
tchuang928 <tc.huang928@gmail.com>
2017-06-06 00:21:06 -0700

    submit contact form

--- src/index.js
+++ src/index.js
@@ -159,7 +159,7 @@ $(document).ready(function() {
 		ajax.onreadystatechange = function() {
 			if(ajax.readyState == 4 && ajax.status == 200) {
 				if(ajax.responseText == "success"){
-					_("contact-form").innerHTML = '<h2>Thanks '+_("n").value+', your message has been sent.</h2>';
+					_("contact-form").innerHTML = '<h2>Thanks '+_("name").value+', your message has been sent.</h2>';
 				} else {
 					_("status").innerHTML = ajax.responseText;
 					_("contact-submit").disabled = false;
