f9559be4bbd8eea3d4da9ce6891a5640b7885ee8
tchuang928 <tc.huang928@gmail.com>
2017-06-05 20:33:38 -0700

    contact form

--- src/index.js
+++ src/index.js
@@ -66,6 +66,7 @@ function topOfPage() {
 }
 
 $(document).ready(() => {
+
 	// check for scroll position in case of browser refresh
 	navbarAnimation();
 	contentSubtitleAnimation();
@@ -92,4 +93,49 @@ $(document).ready(() => {
 		var scroll = $(window).scrollTop();
 		console.log(scroll);
 	});
+});
+
+// contact form
+$(document).ready(function() {
+	// Test for placeholder support
+    $.support.placeholder = (function(){
+        var i = document.createElement('input');
+        return 'placeholder' in i;
+    })();
+
+    // Hide labels by default if placeholders are supported
+    if($.support.placeholder) {
+        $('.form-label').each(function(){
+            $(this).addClass('js-hide-label');
+        });  
+
+        // Code for adding/removing classes here
+        $('.form-group').find('input, textarea').on('keyup blur focus', function(e){
+            
+            // Cache our selectors
+            var $this = $(this),
+                $parent = $this.parent().find("label");
+
+            if (e.type == 'keyup') {
+                if( $this.val() == '' ) {
+                    $parent.addClass('js-hide-label'); 
+                } else {
+                    $parent.removeClass('js-hide-label');   
+                }                     
+            } 
+            else if (e.type == 'blur') {
+                if( $this.val() == '' ) {
+                    $parent.addClass('js-hide-label');
+                } 
+                else {
+                    $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
+                }
+            } 
+            else if (e.type == 'focus') {
+                if( $this.val() !== '' ) {
+                    $parent.removeClass('js-unhighlight-label');
+                }
+            }
+        });
+    } 
 });
